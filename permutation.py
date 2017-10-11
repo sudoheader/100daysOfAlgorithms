e3d8b9ba693280e71cf4a5b80f4c0d16d0b53714
Raymond Nazaryan <rayn2427@gmail.com>
2017-10-10 22:37:03 -0700

    Added permutation.py

---
+++ permutation.py
@@ -0,0 +1,24 @@
+# Next permutation
+def permute(values):
+    n = len(values)
+
+    # i: position of pivot
+    for i in reversed(range(n - 1)):
+        if values[i] < values[i + 1]:
+            break
+    else:
+        # very last permutation
+        values[:] = reversed(values[:])
+        return values
+
+    # j: position of the next candidate
+    for j in reversed(range(i, n)):
+        if values[i] < values[j]:
+            # swap pivot and reverse the tail
+            values[i], values[j] = values[j], values[i]
+            values[i + 1:] = reversed(values[i + 1:])
+            break
+
+    return values
+
+permute(list('FADE'))
