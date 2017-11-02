67fe32e19e7808a39b7c41e1285b162aaa8ead31
Raymond Nazaryan <rayn2427@gmail.com>
2017-11-02 14:45:41 -0700

    Added spiral-matrix.py to return all elements of matrix in spiral order

---
+++ spiral-matrix.py
@@ -0,0 +1,19 @@
+from itertools import count
+
+def spiral(n):
+    matrix = [range(i * n + n)[-n:] for i in range(n)]
+    X, C, R = {}, count(1), matrix
+
+    while R:
+        X.update(zip(R[0], C))
+        R = list(zip(*[i[::-1] for i in R[1:]]))
+
+    return [[X[j] for j in i] for i in matrix]
+
+for i in spiral(3):
+    print('\t'.join(map(str, i)))
+
+for i in spiral(5):
+    print('\t'.join(map(str, i)))
+for i in spiral(7):
+    print('\t'.join(map(str, i)))
